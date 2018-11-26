2ca5a7c5953d47ef600aeb62858698fed96ac449
Issei Takaesu <e185759@ie.u-ryukyu.ac.jp>
2018-11-26 22:36:32 +0900

    a

---
+++ src/main/jp/ac/uryukyu/ie/e185759/report4.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e185759;
+
+public class report4 {
+    public static void main(String[] args){
+        String str = null;
+        System.out.println(str.length());
+    }
+}

1d2ecd07f6e5731a3480e8fe0c01051ff0039374
Issei Takaesu <e185759@ie.u-ryukyu.ac.jp>
2018-11-26 22:48:11 +0900

    a

--- src/main/jp/ac/uryukyu/ie/e185759/report4.java
+++ src/main/jp/ac/uryukyu/ie/e185759/report4.java
@@ -2,7 +2,7 @@ package jp.ac.uryukyu.ie.e185759;
 
 public class report4 {
     public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 }
